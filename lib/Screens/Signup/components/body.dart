465681c6c23b3b95269339995bffa29ce3a07de0
unknown <chethiyanishanath@gmail.com>
2020-11-07 04:35:16 +0530

    Added login funtionalities

--- lib/Screens/Signup/components/body.dart
+++ lib/Screens/Signup/components/body.dart
@@ -7,81 +7,125 @@ import 'package:flutter_auth/components/already_have_an_account_acheck.dart';
 import 'package:flutter_auth/components/rounded_button.dart';
 import 'package:flutter_auth/components/rounded_input_field.dart';
 import 'package:flutter_auth/components/rounded_password_field.dart';
+import 'package:flutter_auth/firebaseDB/auth.dart';
 import 'package:flutter_auth/grid_view/categories.dart';
 import 'package:flutter_auth/profile/user_profile.dart';
 import 'package:flutter_svg/svg.dart';
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
+
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+
+  String email;
+  String password;
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Background(
       child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // Text(
-            //   "SIGNUP",
-            //   style: TextStyle(fontWeight: FontWeight.bold),
-            // ),
-            SizedBox(height: size.height * 0.03),
-            SvgPicture.asset(
-              "assets/icons/signup.svg",
-              height: size.height * 0.35,
-            ),
-            RoundedInputField(
-              hintText: "Your Email",
-              onChanged: (value) {},
-            ),
-            RoundedPasswordField(
-              onChanged: (value) {},
-            ),
-            RoundedButton(
-              text: "SIGNUP",
-              press: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return UserProile();
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // Text(
+              //   "SIGNUP",
+              //   style: TextStyle(fontWeight: FontWeight.bold),
+              // ),
+              SizedBox(height: size.height * 0.03),
+              SvgPicture.asset(
+                "assets/icons/signup.svg",
+                height: size.height * 0.35,
+              ),
+              RoundedInputField(
+                hintText: "Your Email",
+                onChanged: (value) {
+                  setState(() {
+                    this.email = value;
+                  });
+                },
+              ),
+              RoundedPasswordField(
+                onChanged: (value) {
+                  setState(() {
+                    this.password = value;
+                  });
+                },
+              ),
+              RoundedButton(
+                text: "SIGNUP",
+                press: () async{
+                  print(this.email);
+                  print(this.password);
+                  try{
+                    var response = await registerUser(
+                        email: this.email,
+                        pass: this.password);
+                    print(response);
+                  }catch(err){
+                    print(err.toString());
+                  }
+//                  Navigator.push(
+//                    context,
+//                    MaterialPageRoute(
+//                      builder: (context) {
+//                        return UserProile();
+//                      },
+//                    ),
+//                  );
+                },
+              ),
+              SizedBox(height: size.height * 0.03),
+              AlreadyHaveAnAccountCheck(
+                login: false,
+                press: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return LoginScreen();
+                      },
+                    ),
+                  );
+                },
+              ),
+              OrDivider(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SocalIcon(
+                    iconSrc: "assets/icons/facebook.svg",
+                    press: () async{
+                      print("facebook login");
                     },
                   ),
-                );
-              },
-            ),
-            SizedBox(height: size.height * 0.03),
-            AlreadyHaveAnAccountCheck(
-              login: false,
-              press: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return LoginScreen();
+                  SocalIcon(
+                    iconSrc: "assets/icons/google-plus.svg",
+                    press: () async{
+                      try{
+                        var response = await signInWithGoogle();
+                      }catch(err){
+                        print(err.toString());
+                      }
                     },
                   ),
-                );
-              },
-            ),
-            OrDivider(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                SocalIcon(
-                  iconSrc: "assets/icons/facebook.svg",
-                  press: () {},
-                ),
-                SocalIcon(
-                  iconSrc: "assets/icons/google-plus.svg",
-                  press: () {},
-                ),
-                SocalIcon(
-                  iconSrc: "assets/icons/apple.svg",
-                  press: () {},
-                ),
-              ],
-            )
-          ],
+                  SocalIcon(
+                    iconSrc: "assets/icons/apple.svg",
+                    press: () async{
+                      print("icloud login");
+                    },
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
